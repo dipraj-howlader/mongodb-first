06481679fd4fccd30be15d276667dbfbb5d655e2
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-03-25 01:33:07 +0600

    adf

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
+
+const uri = "mongodb+srv://organicUser:DXJU2u3UR5.JEvp@cluster0.k0zxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const app = express();
 
-const password = DXJU2u3UR5.JEvp;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.get('/', (req, res) => {
     res.send ("aaa")
 })
